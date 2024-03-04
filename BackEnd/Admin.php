1ccf76ac3949ff434fff8007231b71b7f8e9a507
Blitz576 <mo7medismail200@gmail.com>
2024-03-04 20:37:28 +0200

    Curd Admins

--- BackEnd/Admin.php
+++ BackEnd/Admin.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     $current = 'Admins';
     $id = 1 ;
     include "include/sidebar.php";
@@ -6,18 +7,21 @@
     include "DataBase/DBCLass.php";
     use DbClass\Table;
     $admins = new Table('admins');
-    // $cond = "user_role".'='."'0'";
-    // $result = $admins->FindAll($cond);
-    // if(isset($_GET['add']) == 'Admin'){
-    //     include "Admins/AddForm.php";  
-    // }
-    // elseif(isset($_GET['edit'])){
-    //     $AdminId = $_GET['edit'];
-    //     $SelAdmin = $admins->FindById('user_id',$AdminId);
-    //     include "Admins/EditForm.php";
-    // }else{
-    //     include "Admins/table.php";
-    // }
+    $col = ['id','username','email','profile_picture','created_at'];
+    $result = $admins->Select($col);
+    
+    if(isset($_GET['add']) == 'Admin'){
+        include "Admins/AddForm.php";  
+    }
+    elseif(isset($_GET['edit'])){
+        $AdminId = $_GET['edit'];
+        $cond = "id = $AdminId";
+        $SelAdmin = $admins->Select($col,$cond);
+        $SelAdmin = $SelAdmin[0];
+        include "Admins/EditForm.php";
+    }else{
+        include "Admins/table.php";
+    }
     
     include "include/footer.php";
 ?>
