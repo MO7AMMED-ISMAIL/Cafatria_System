47886531ed86a02731e33f6688f74d278d7ed36e
Blitz576 <mo7medismail200@gmail.com>
2024-03-06 13:44:25 +0200

    New Project

--- BackEnd/API/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
-    header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
-
-
-?>
--- BackEnd/API/users/addUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $username = $_POST['username'];
-    echo $username;
-}
-
-?>
