76b73c810beab174a12c0e1af2a423fcc8cabbbd
Blitz576 <mo7medismail200@gmail.com>
2024-03-06 15:21:33 +0200

    file Structure

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -1,12 +1,23 @@
 <?php 
+    $current = "index";
     include "include/sidebar.php";
     include "include/navbar.php";
 ?>
 
+<div class="container-fluid">
 <div class="row">
-    <div class="col">
-        <p>Page content goes here</p>
-    </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow ">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <h3 class="mb-1 names">Number of Users-><span>&nbsp;4</span></h3>
+                            <p><a class="users" href="users.html">View Details</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
 
 <?php 
