9e31ec6dc82a81d85ad7ac81ed1e39f44d4fe3a2
Blitz576 <mo7medismail200@gmail.com>
2024-03-06 15:31:48 +0200

    File Structure

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -5,21 +5,61 @@
 ?>
 
 <div class="container-fluid">
-<div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow ">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <h3 class="mb-1 names">Number of Users-><span>&nbsp;4</span></h3>
-                            <p><a class="users" href="users.html">View Details</a></p>
+    <div class="row">
+            <!-- Users -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow ">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2 text-center">
+                                <h3 class="mb-1 names">Users-><span>&nbsp;4</span></h3>
+                                <p><a class="users mt-3">View Details</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <!-- Admims -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow ">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2 text-center">
+                                <h3 class="mb-1 names">Admin-><span>&nbsp;4</span></h3>
+                                <p><a class="users mt-3">View Details</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Orders -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow ">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2 text-center">
+                                <h3 class="mb-1 names">Orders-><span>&nbsp;4</span></h3>
+                                <p><a class="users mt-3">View Details</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Checks -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow ">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2 text-center">
+                                <h3 class="mb-1 names">Users-><span>&nbsp;4</span></h3>
+                                <p><a class="users mt-3">View Details</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
 </div>
-
 <?php 
     include "include/footer.php";
 ?>
