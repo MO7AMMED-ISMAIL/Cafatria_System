163ce839c8c45df7c14fd19abccb812494d9d458
Blitz576 <mo7medismail200@gmail.com>
2024-03-10 13:43:07 +0200

    room table database

--- BackEnd/DataBase/cafeteria.sql
+++ BackEnd/DataBase/cafeteria.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 08, 2024 at 12:33 PM
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2024 at 12:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,7 +42,8 @@ CREATE TABLE `admins` (
 
 INSERT INTO `admins` (`id`, `username`, `email`, `password`, `profile_picture`, `created_at`) VALUES
 (1, 'admin1', 'admin1@gmail.com', '123456', 'user.png', '2024-03-08 11:33:06'),
-(2, 'admin2', 'admin2@gmail.com', '123456', 'user.png', '2024-03-08 11:33:06');
+(2, 'admin2', 'admin2@gmail.com', '123456', 'user.png', '2024-03-08 11:33:06'),
+(3, 'mo7ismail88', 'fcisdodo900@gmail.com', '12345678', 'user.png', '2024-03-08 16:38:41');
 
 -- --------------------------------------------------------
 
@@ -103,8 +104,8 @@ CREATE TABLE `orders` (
   `tax` float NOT NULL DEFAULT 0.1,
   `total_price_after_tax` float NOT NULL,
   `status` enum('Processing','Out For Delivery','Done','Cancelled') DEFAULT 'Processing',
+  `room` int(11) NOT NULL,
   `notes` text DEFAULT NULL,
-  `room_number` enum('Cafeteria','Room One (R1)','Room Two (R2)','Room Three (R3)','Room Four (R4)') DEFAULT 'Cafeteria',
   `order_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -112,9 +113,10 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `total_price`, `tax`, `total_price_after_tax`, `status`, `notes`, `room_number`, `order_date`) VALUES
-(1, 1, 8, 0.1, 8.8, 'Done', 'Special instructions for the order', 'Cafeteria', '2024-03-08 11:33:07'),
-(2, 2, 3.5, 0.1, 3.85, 'Processing', NULL, 'Cafeteria', '2024-03-08 11:33:07');
+INSERT INTO `orders` (`id`, `user_id`, `total_price`, `tax`, `total_price_after_tax`, `status`, `room`, `notes`, `order_date`) VALUES
+(1, 1, 8, 0.1, 8.8, 'Done', 1, 'Special instructions for the order', '2024-03-08 11:33:07'),
+(2, 2, 3.5, 0.1, 3.85, 'Processing', 1, NULL, '2024-03-08 11:33:07'),
+(3, 2, 6.5, 0.1, 0.65, 'Processing', 2, 'fwrgvtebt3b', '2024-03-08 21:06:08');
 
 -- --------------------------------------------------------
 
@@ -139,7 +141,9 @@ CREATE TABLE `order_items` (
 INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `product_price`, `quantity`, `total_price`, `created_at`) VALUES
 (1, 1, 1, 2.5, 2, 5, '2024-03-08 11:33:07'),
 (2, 1, 2, 1.5, 1, 1.5, '2024-03-08 11:33:07'),
-(3, 2, 3, 5, 1, 5, '2024-03-08 11:33:07');
+(3, 2, 3, 5, 1, 5, '2024-03-08 11:33:07'),
+(4, 3, 1, 2.5, 2, 5, '2024-03-08 21:06:08'),
+(5, 3, 2, 1.5, 2, 3, '2024-03-08 21:06:08');
 
 -- --------------------------------------------------------
 
@@ -170,6 +174,26 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `picture`, `catego
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `room_number` varchar(255) NOT NULL,
+  `ext` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `room_number`, `ext`) VALUES
+(1, 'room1', 'info room1'),
+(2, 'room2', 'info room2');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -178,8 +202,6 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `room_number` enum('Cafeteria','Room One (R1)','Room Two (R2)','Room Three (R3)','Room Four (R4)') DEFAULT 'Cafeteria',
-  `extra_data` varchar(255) DEFAULT NULL,
   `profile_picture` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -188,9 +210,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `room_number`, `extra_data`, `profile_picture`, `created_at`) VALUES
-(1, 'john_doe', 'user1@gmail.com', '123456', 'Cafeteria', 'Some extra data', 'user.png', '2024-03-08 11:33:06'),
-(2, 'jane_smith', 'user2@gmail.com', '123456', 'Cafeteria', 'Additional info', 'user.png', '2024-03-08 11:33:06');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `profile_picture`, `created_at`) VALUES
+(1, 'john_doe', 'user1@gmail.com', '123456', 'user.png', '2024-03-08 11:33:06'),
+(2, 'jane_smith', 'user2@gmail.com', '123456', 'user.png', '2024-03-08 11:33:06');
 
 --
 -- Indexes for dumped tables
@@ -222,7 +244,8 @@ ALTER TABLE `notifications`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `room` (`room`);
 
 --
 -- Indexes for table `order_items`
@@ -240,6 +263,12 @@ ALTER TABLE `products`
   ADD KEY `category_id` (`category_id`);
 
 --
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -255,7 +284,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -273,19 +302,25 @@ ALTER TABLE `notifications`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -307,7 +342,8 @@ ALTER TABLE `notifications`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`room`) REFERENCES `rooms` (`id`);
 
 --
 -- Constraints for table `order_items`
