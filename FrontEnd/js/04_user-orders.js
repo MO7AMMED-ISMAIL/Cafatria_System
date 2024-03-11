15a070a5586e88419016324be337a62dbe7c1097
Blitz576 <mo7medismail200@gmail.com>
2024-03-11 17:27:20 +0200

    fatma part

--- FrontEnd/js/04_user-orders.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function () {
-  $('.fa-plus-square').on('click', function () {
-      $(this).closest('tr.order').nextUntil('tr.order').toggleClass('details-hidden');
-  });
-});
